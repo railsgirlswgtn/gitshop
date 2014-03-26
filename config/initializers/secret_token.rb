57ae5ea89745b6e36cafbfde77a92419309ec3dc
Mathew Hartley <matt@route66.sytes.net>
2014-03-26 22:28:55 +1300

    Gitshop initial commit
    This is a rails 4.0.3 new project.
    Turbolinks has been removed, and a welcome#index action generated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitshop::Application.config.secret_key_base = '3b392bb48fc114db57bc8f4a1d09ae009bbfd3a480187cb3b49b9b5375f0c9e7f28525bed22f83cf57bf89ae13ad2a932c93e990b89df2e50e86999bf0938e78'
