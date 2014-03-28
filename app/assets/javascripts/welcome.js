968207f272598b6d503c389502bbb84281b2f573
Eoin Kelly <eoin@rabidtech.co.nz>
2014-03-28 15:06:21 +1300

    Reset CSS and HTML to starting point for students
    * Remove CSS and JSS (but leave files)
    * Remove class names from HTML

--- app/assets/javascripts/welcome.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-  $(document).on('click', '.js-enlarge-on-click', function(e) {
-
-    $('.js-enlarge-on-click').removeClass('selected');
-    $(e.currentTarget).addClass('selected');
-
-  });
-});
